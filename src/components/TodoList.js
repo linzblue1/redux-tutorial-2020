ecdf18ae2eea786bd0a221c8c82f8211a2014973
Blue Lindsey <linzblue5@gmail.com>
2020-04-06 10:55:45 -0500

    Init commit, Todo's adding, but can't b removed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { connect } from "react-redux"
+import { getTodos } from "../redux/selectors"
 import Todo from "./Todo";
 
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
     {todos && todos.length
       ? todos.map((todo, index) => {
-          return <Todo key={`todo-${todo.id}`} todo={todo} />;
-        })
+        return <Todo key={`todo-${todo.id}`} todo={todo} />;
+      })
       : "No todos, yay!"}
   </ul>
 );
 
-export default TodoList;
+const mapStateToProps = state => {
+  const { byIds, allIds } = state.todos || {};
+  const todos =
+    allIds && allIds.length
+      ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+      : null;
+  return { todos };
+}
+
+export default connect(state => ({ todos: getTodos(state) }))(TodoList);
